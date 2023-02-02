f3c138dbeb9741e56beac0a1bb34edd21434c49a
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-02 10:25:15 +0200

    added minor style tweaks

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ValidationForm } from '@aleksasdev/validation-form';
 
 export const Register = () => {
    return (
       <section id="register">
-         
+         <ValidationForm label="Register" >
+
+         </ValidationForm>
       </section>
    )
 }
