3d702788411c417aa428d1278fbe123dca53a379
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-02 10:17:05 +0200

    stylized navbar

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -6,6 +6,7 @@ export const Navbar = () => {
    return (
       <>
          <nav>
+            <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
          </nav>
